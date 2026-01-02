df6e50f860f387b50948acbf191b0923a0bb8c8f
Joshua Olagunju <olagunjujoshua11@gmail.com>
2026-01-02 23:41:01 +0100

    first commit

--- src/pages/header.tsx
+++ src/pages/header.tsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 export function Header() {
   const [mobileBar, setMobileBar] = useState(false);
   return (
-    <header className="sticky top-0 z-50 bg-[#275b8f] !px-5 !md:px-20 py-5 md:py-0   ">
+    <header className="sticky top-0 z-50 bg-[#275b8f] px-5 md:px-20 py-5 md:py-0">
       <div className="flex justify-between items-center ">
         <div className="font-[500] text-[#eafab4] text-lg">JOSHUA .O</div>
         <ul className="hidden md:flex text-[#eafab4] cursor-pointer font-[500] text-sm flex h-16 space-x-4  py-10 gap-10 items-center justify-center">
@@ -35,23 +35,36 @@ export function Header() {
         />
 
         <MdOutlineMenu
-          className="text-3xl text-[#eafab4]   md:hidden cursor-pointer"
+          className="text-3xl text-[#eafab4]   md:hidden cursor-pointer border-2 p-1 rounded-md"
           onClick={() => setMobileBar((prev) => !prev)}
         />
-
-       
       </div>
-       {mobileBar && (
-          <div>
-            {/* Mobile Menu can be implemented here */}
-            <ul className="flex flex-col bg-[#275b8f] text-white !mt-5 gap-4" onClick={() => setMobileBar(false)}>
-              <li>   <a href="#home">Home</a></li>
-              <li>   <a href="#about">About</a></li>
-              <li>   <a href="#projects">Projects</a></li>
-              <li>   <a href="#experience">Experience</a></li>
-            </ul>
-          </div>
-        )}
+      {mobileBar && (
+        <div className="absolute top-17 left-0 right-0 z-40 transition-all duration-300 origin-top">
+          {/* Mobile Menu can be implemented here */}
+          <ul
+            className="flex flex-col font-[400] bg-[#275b8f] border-t border-[#eafab4] text-white text-sm gap-5  pt-5  pb-5 px-5 md:px-20"
+            onClick={() => setMobileBar(false)}
+          >
+            <li>
+              {" "}
+              <a href="#home">HOME</a>
+            </li>
+            <li>
+              {" "}
+              <a href="#about">ABOUT</a>
+            </li>
+            <li>
+              {" "}
+              <a href="#projects">PROJECTS</a>
+            </li>
+            <li>
+              {" "}
+              <a href="#experience">EXPERIENCE</a>
+            </li>
+          </ul>
+        </div>
+      )}
     </header>
   );
 }
